aa9302f324550ea6d6359f057a7f4e8b2245f695
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-05-30 21:18:56 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,18 @@ import { useState } from "react";
 
 export const App = () => {
   const [url, setUrl] = useState('');
-  const [receiptId, setReceiptId] = useState('');
+
 
   const handleChange = (e) => {
     setUrl(e.target.value);
   };
 
   const extractReceiptId = () => {
-    // Удаляем последний фрагмент, если это '/pdf'
     const urlWithoutPdf = url.endsWith('/pdf') ? url.slice(0, -4) : url;
     const id = urlWithoutPdf.split('/').filter(Boolean).pop();
-    setReceiptId(id);
+
     setUrl("");       
 
-    // Переход по ссылке
     if (id) {
       window.location.href = `https://check.checkbox.ua/${id}`;
     }
