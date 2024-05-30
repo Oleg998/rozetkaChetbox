2471aab22a65bbd4c6caa448dc47dc12628bc36b
Oleg998 <137883971+Oleg998@users.noreply.github.com>
2024-05-30 20:49:27 +0300

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { useState } from "react";
+
 export const App = () => {
+  const [url, setUrl] = useState('');
+  const [receiptId, setReceiptId] = useState('');
+
+  const handleChange = (e) => {
+    setUrl(e.target.value);
+  };
+
+  const extractReceiptId = () => {
+    // Удаляем последний фрагмент, если это '/pdf'
+    const urlWithoutPdf = url.endsWith('/pdf') ? url.slice(0, -4) : url;
+    const id = urlWithoutPdf.split('/').filter(Boolean).pop();
+    setReceiptId(id);
+    setUrl("");       
+
+    // Переход по ссылке
+    if (id) {
+      window.location.href = `https://check.checkbox.ua/${id}`;
+    }
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className="App">
+      <h1>Просмотр чека</h1>
+      <input 
+        type="text" 
+        placeholder="Введите URL чека" 
+        value={url}
+        onChange={handleChange}
+      />
+      <button onClick={extractReceiptId}>Получить ID чека</button>
     </div>
   );
 };
